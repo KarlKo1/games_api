f6bcfbc3fa06728600aeb64674af0d46c3733d09
kongopoiss@gmail.com <kongopoiss@gmail.com>
2022-01-31 11:23:42 +0200

    As an app developer I want to get a list of all games to show in my app [Delivers #181056333]

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8080
 app.get('/games', (req, res) => {
     res.send(["Witcher 3", "Cyberpunk 2077"])
 })
-
+app.get
 app.listen(port, () => {
     console.log(`API is up at: http://localhost:${port}`)
 })
